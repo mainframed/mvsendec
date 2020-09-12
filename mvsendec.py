3c3a3b95b43c4141a125ac57a1eff07b0fe0c97b
mainframed <mainframed767@gmail.com>
2020-09-11 22:08:47 -0700

    shhhhhhhhhhh

--- mvsendec.py
+++ mvsendec.py
@@ -6,6 +6,20 @@
 # GPL v3
 
 
+JCLHEADER = '''//ENCODE   JOB CLASS=A,MSGCLASS=H,MSGLEVEL=(1,1),
+//         USER=HERC01,PASSWORD=CUL8TR
+//MVSENDEC EXEC PGM=MVSENDEC,PARM='decb dd:in dd:out'
+//STEPLIB  DD  DSN=SYS2.LINKLIB,DISP=SHR
+//IN       DD  *'''
+
+JCLFOOTER = '''/*
+//OUT      DD  DSN=PHIL.JCLLIB(ZIPENDEC),
+//SYSPRINT DD  SYSOUT=*
+//SYSTERM  DD  SYSOUT=*
+//SYSIN    DD  DUMMY
+//*'''
+
+
 import sys
 import ebcdic
 
@@ -20,10 +34,14 @@ arg = sys.argv[1].lower()
 fp = sys.argv[2]
 fq = sys.argv[3]
 
-if len(sys.argv) == 5:
+if len(sys.argv) >= 5:
     fp = sys.argv[3]
     fq = sys.argv[4]
     lrecl = int(sys.argv[2])
+
+if len(sys.argv) == 6:
+    # whatever it is we're doing JCL
+    shhhhhh_its_a_secret = True
 
 if arg in ['encb', 'decb', 'dect']:
     with open(fp, 'rb') as f:
@@ -57,3 +75,9 @@ if arg in ['encb','enct', 'dect']:
 else:
     with open(fq, 'wb') as f:
             f.write(outbuf)
+
+if shhhhhh_its_a_secret and arg in ['encb','enct']:
+    print(JCLHEADER)
+    for item in outbuf:
+        print(item)
+    print(JCLFOOTER)
